b36694c226b302f74b3e2ea0cee3e0946ec37174
losnah-think <hansol416@naver.com>
2026-01-12 23:59:23 +0900

    Configure basePath for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
   },
   // 만약 저장소 이름이 username.github.io 라면 basePath를 주석 처리하세요.
   // 만약 저장소 이름이 프로젝트 명(예: homepage)이라면 아래 주석을 해제하고 프로젝트 이름을 적어주세요.
-  // basePath: '/homepage',
+  basePath: '/homepage',
 };
 
 export default nextConfig;
